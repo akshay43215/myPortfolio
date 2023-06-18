6467a6c00ce2a908b866356b165b3afb096b319b
akshay43215 <akshaykrishna24996@gmail.com>
2023-06-18 18:32:18 +0530

    favicon added

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MariyamResume from "../../assets/Akshay-Krishna.pdf";
+import MyResume from "../../assets/Akshay-krishna(web-developer).pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={MariyamResume} download className="btn">
+      <a href={MyResume} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
